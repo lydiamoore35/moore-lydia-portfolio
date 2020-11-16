c39b11ebe0239dfd203250dfecb6d855a6e75b23
Lydia Moore <lydiamoore35@gmail.com>
2020-11-15 20:09:04 -0700

    need to get layout page and other pages showing up again

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,50 +4,24 @@
  *
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
-
+// import PropTypes from "prop-types"
+// import { useStaticQuery, graphql } from "gatsby"
+// import Header from "./header"
 import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
 import "./layout.css"
+// import IndexPage from "../pages/index.js"
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
+const Layout = () => {
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer style={{
-          marginTop: `2rem`
-        }}>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
+      <div>
+        <header>this is my header</header>
+        <main></main>
+        <footer>
         </footer>
       </div>
     </>
   )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
 }
 
 export default Layout
