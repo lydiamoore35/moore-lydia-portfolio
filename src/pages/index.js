7ca0b6ebe591b6310a415ddb9ddd997b1b6c3b02
Lydia Moore <lydiamoore35@gmail.com>
2020-11-23 12:23:39 -0700

    second porject added but having issues with the text box

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ const IndexPage = () => (
         </div>
       <h3 class="AboutTitle">ABOUT</h3>
       <div class="AboutDetails">
-      Hello, my name is Lydia and I am a modern software engineer seeking opportunity in the programming world. For the past 5 months I have been completing General Assembly’s Software Engineering Immersive course for full-stack development. My previous experience involving<br></br><br></br>I have embraced my keen attention to detail and goal completion determination to accomplish multiple projects to their fullest extent. I practice effective time allocation and maintain a detail-oriented workflow allowing me the necessary time to anticipate the needs of team members and instructors. I aspire to establish myself as a developer within a reputable and mission-driven company that will actively cultivate the strengths of its creators.
+      My name is Lydia and I am a full-stack software engineer based in Colorado. Building new applications or enhancing preexisting code is how I express my keen attention to detail and relish in the satisfaction of completing tasks to their fullest extent. My previous experience involving behavioral assessments and transitioning data to mainstream technology has given me a unique perspective on interacting within the coding world.<br></br><br></br>I practice effective time allocation and maintain a detail-oriented workflow allowing me the necessary time to anticipate the needs of clients and team members. I aspire to establish myself as a developer within a reputable and mission-driven company that will actively cultivate the strengths of its creators.
       </div>
       <div class="Skills" id="SkillsLink">
       <h3 class="TechnicalSkills">TECHNICAL SKILLS</h3>
@@ -65,10 +65,7 @@ const IndexPage = () => (
             <img src="https://img.icons8.com/color/96/000000/mongodb.png" alt="icon"/>
           </li>
           <li>
-            <img src="https://img.icons8.com/color/96/000000/react-native.png" alt="icon" height="96"/>
-          </li>
-          <li>
-            <img src="https://pbs.twimg.com/profile_images/687314343537569792/Hz7m5vRM_400x400.png" alt="icon" height="96"/>
+            <img src="https://img.icons8.com/color/96/000000/react-native.png" alt="react" height="96"/>
           </li>
           <li>
             <img src="https://www.sketchappsources.com/resources/source-image/sketch-icon-41.png" atl="icon" height="96"/>
@@ -77,7 +74,7 @@ const IndexPage = () => (
             <img src="https://i.pinimg.com/280x280_RS/0f/b8/e6/0fb8e676a1cd0eae9b0f7ea862c40f93.jpg" alt="icon" height="96"/>
           </li>
           <li>
-            <img src="https://cdn.auth0.com/blog/build-a-secure-express-api-using-postman-and-auth0/postman-logo.png" alt ="icon" height="96"/>
+            <img src="https://cdn.auth0.com/blog/build-a-secure-express-api-using-postman-and-auth0/postman-logo.png" alt ="postman" height="96"/>
           </li>
           <li>
             <img src="https://miro.medium.com/max/502/1*a8vazziKqAGfX1FGObH95Q.png" alt="icon" height="96"/>
@@ -86,7 +83,10 @@ const IndexPage = () => (
             <img src="https://pbs.twimg.com/profile_images/1021778918842531840/FBVP_uDf.jpg" alt="Trello" height="96"/>
           </li>
           <li>
-            <img src="https://i.dlpng.com/static/png/432835_preview.png" alt="icon" height="96"/>
+            <img src="https://pbs.twimg.com/profile_images/687314343537569792/Hz7m5vRM_400x400.png" alt="milligram" height="96"/>
+          </li>
+          <li>
+            <img src="https://img.icons8.com/windows/64/000000/bootstrap.png" alt="bootstrap" height="96"/>
           </li>
         </ul>
       </div>
@@ -100,6 +100,16 @@ const IndexPage = () => (
             <button class="SiteButton">Visit Site</button>
             <div class="Project_3_text">
               <u><b>Ubuntu</b></u><br></br>I am because we are<br></br><br></br>Ubuntu is a full CRUD application that uses MERN stack technoglogy. I used the models, views, controllers setup with sign up and log in functionality. I deployed Ubuntu front end and back end through Netlify and Heroku, respectively.
+            </div>
+          </div>
+        </div>
+        <div class="Project_2">
+          <img src="https://i.imgur.com/87ab3al.png?2" alt="project_2_pic"/>
+          <div class="center_project_2">
+            <button class="GitButton">GitHub</button>
+            <button class="SiteButton">Visit Site</button>
+            <div class="Project_2_text">
+              <u><b>Create Your Cookbook</b></u><br></br><br></br>I built WeatherWize using HTML, CSS, JavaScript and JQuery, hosted on github pages. I used AJAX to request a weather API based on zip code. Once the user inputs their zipcode, the browser renders youth activities based on the tempurature. WeatherWize has a responsive design and complex user interface.
             </div>
           </div>
         </div>
