fc465d636d1e7d9460ffd05b31433a1926a184f7
Lydia Moore <lydiamoore35@gmail.com>
2020-11-23 09:40:16 -0700

    more css and fixed the buttons to so ready to go and changed order of projects to render on page

--- src/pages/index.js
+++ src/pages/index.js
@@ -93,6 +93,16 @@ const IndexPage = () => (
     </div>
       <h3 class="Projects" id="ProjectsLink">PROJECTS</h3>
         <div></div>
+        <div class="Project_3">
+          <img src="https://i.imgur.com/yFYcW1H.png" alt="project_3_pic"/>
+          <div class="center">
+            <button class="GitButton">GitHub</button>
+            <button class="SiteButton">Visit Site</button>
+            <div class="Project_3_text">
+              <u><b>Ubuntu</b></u><br></br>I am because we are<br></br><br></br>Ubuntu is a full CRUD application that uses MERN stack technoglogy. I used the models, views, controllers setup with sign up and log in functionality. I deployed Ubuntu front end and back end through Netlify and Heroku, respectively.
+            </div>
+          </div>
+        </div>
         <div class="Project_1">
           <img src="https://i.imgur.com/hs6xZvs.png" alt="project_1_pic"/>
           <div class="center">
@@ -100,16 +110,6 @@ const IndexPage = () => (
             <button class="SiteButton">Visit Site</button>
             <div class="Project_1_text">
               <u><b>WeatherWize</b></u><br></br><br></br>I built WeatherWize using HTML, CSS, JavaScript and JQuery, hosted on github pages. I used AJAX to request a weather API based on zip code. Once the user inputs their zipcode, the browser renders youth activities based on the tempurature. WeatherWize has a responsive design and complex user interface.
-            </div>
-          </div>
-        </div>
-        <div class="Project_3">
-          <img src="https://i.imgur.com/yFYcW1H.png" alt="project_3_pic"/>
-          <div class="center">
-            <button class="GitButton">GitHub</button>
-            <button class="SiteButton">Visit Site</button>
-            <div class="Project_3_text">
-              <u><b>Ubuntu</b></u><br></br>I am because we are<br></br><br></br>Ubuntu is an application that allows users to sign up and log in to thier account to create, view, update, and/or delete community projects in thier area.<br></br>Ubuntu includes front end and back end application through Heroku and Netlify.
             </div>
           </div>
         </div>
