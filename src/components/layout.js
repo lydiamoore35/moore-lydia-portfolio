c43fa91a8cec451107363125933fce4b6056ce4c
Lydia Moore <lydiamoore35@gmail.com>
2020-12-27 16:49:20 -0700

    cleaning code

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,7 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-// import { useStaticQuery, graphql } from "gatsby"
-// import Header from "./header"
+
 import PropTypes from "prop-types"
 import React from "react"
 import "./layout.css"
-// import IndexPage from "../pages/index.js"
 
 const Layout = ({children}) => {
   return (
