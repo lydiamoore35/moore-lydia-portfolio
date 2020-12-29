3fa5188b5ee938d20094414d2d672b0e0c1e8d14
Lydia Moore <lydiamoore35@gmail.com>
2020-12-29 15:56:43 -0700

    cleaning code

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,10 +1,3 @@
-/**
- * SEO component that queries for data with
- *  Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
@@ -34,30 +27,6 @@ function SEO({ description, lang, meta, title }) {
       title={title}
       titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : null}
       meta={[
-        // {
-        //   name: `description`,
-        //   content: metaDescription,
-        // },
-        // {
-        //   property: `og:title`,
-        //   content: title,
-        // },
-        // {
-        //   property: `og:description`,
-        //   content: metaDescription,
-        // },
-        // {
-        //   property: `og:type`,
-        //   content: `website`,
-        // },
-        // {
-        //   name: `twitter:card`,
-        //   content: `summary`,
-        // },
-        // {
-        //   name: `twitter:title`,
-        //   content: title,
-        // },
       ].concat(meta)}
     />
   )
